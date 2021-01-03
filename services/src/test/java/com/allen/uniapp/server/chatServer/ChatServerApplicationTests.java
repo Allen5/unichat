95f9cba297e49d45ec9aaa74e99ba1daebd1bf09
三爷 <allenlikeu@gmail.com>
2021-01-03 16:14:19 +0800

    init: 初始化服务端工程

---
+++ services/src/test/java/com/allen/uniapp/server/chatServer/ChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allen.uniapp.server.chatServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
